568b508cdcc2bdf081fe9f5cc3b99b1abc1c548f
dreylopez <dreylopez@eniac19.danielcastelao.org>
2015-01-26 09:19:08 +0100

    Más commints

--- src/bcontornos/Bcontornos.java
+++ src/bcontornos/Bcontornos.java
@@ -14,6 +14,7 @@ public class Bcontornos {
     /**
      * @param args the command line arguments
      */
+    //Holaaa
     public static void main(String[] args) {
         // TODO code application logic here
         Contornos obx =new Contornos();
